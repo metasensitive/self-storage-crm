b911d9ca369bcbb8c42b82d4bb8a20dc225034d1
sadpoetic <zloychannel19@gmail.com>
2026-04-27 19:44:45 +0600

    fix(backend): убрал ошибочные автоимпорты из моделей

--- backend/app/Models/Container.php
+++ backend/app/Models/Container.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Carbon\Traits\Units;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
