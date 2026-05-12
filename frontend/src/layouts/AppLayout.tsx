b2d230e6abbfce047f8a08cc10c8a35fab910443
metasensitive <zloychannel19@gmail.com>
2026-05-12 21:33:59 +0600

    chore(frontend): удалена избыточная плавающая панель tweaks

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from '@/components/Sidebar';
-import { TweaksPanel } from '@/components/TweaksPanel';
 
 export function AppLayout() {
   return (
@@ -9,7 +8,6 @@ export function AppLayout() {
       <main className="main">
         <Outlet />
       </main>
-      <TweaksPanel />
     </div>
   );
 }
